323af07ba8dcd9973305409ec3312b8d73115dd7
Adepeju18 <73551379+Adepeju18@users.noreply.github.com>
2021-01-17 15:56:47 +0000

    js2.week2 exercises

--- exercises/exercise.js
+++ exercises/exercise.js
@@ -18,6 +18,15 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+let allParaghaph =document.getElementsByTagName("p");
+console.log(allParaghaph.length);
+let firstDiv = document.querySelector("div");
+console.log(firstDiv);
+let jumbo = document.getElementById("jumbotron-text");
+console.log(jumbo);
+let content = document.getElementsByClassName("primary-content");
+let para = content[0].getElementsByTagName("p");
+console.log(para.length);
 
 /*
 Task 2
@@ -25,7 +34,8 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
-
+// let alertButton = document.getElementById("alertBtn");
+// alertButton.addEventListener("click",alert("Thanks for visiting Bikes for Refugees!"));
 
 /*
 Task 3
@@ -33,7 +43,11 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
+let chgButton = document.getElementById("bgrChangeBtn");
+chgButton.addEventListener("click",function(){
+    document.body.style.backgroundColor = "yellow";
 
+});
 
 /*
 Task 4
@@ -41,6 +55,14 @@ Task 4
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
+let myButton = document.getElementById("addTextBtn");
+myButton.addEventListener("click",function(){
+    let newArticle = document.createElement("p");
+     newArticle.innerText = "Read more below";
+     let button = document.getElementById("hello");
+     button.appendChild(newArticle);
+    
+});
 
 
 
@@ -50,3 +72,8 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+let largerBtn = document.getElementById("largerLinksBtn");
+largerBtn.addEventListener("click",function(){
+  let button = document.getElementById("hello");
+  hello.style.fontSize="2em";
+});
