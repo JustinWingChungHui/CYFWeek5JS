3cbf62b32e02b0ada81c0d7674286904fb3b890a
Justin Wing Chung Hui <info@techluminaire.com>
2021-02-09 16:45:22 +0000

    Removed Answers

--- exercises/exercise.js
+++ exercises/exercise.js
@@ -20,17 +20,6 @@ Write JavaScript below that logs:
 */
 
 
-let paragraphs = document.querySelectorAll('p');
-console.log(paragraphs);
-
-let firstDiv = document.querySelector('div');
-console.log(firstDiv);
-
-let jumbotronText = document.getElementById('jumbotron-text');
-console.log(jumbotronText);
-
-let pInsidePrimaryContent = document.querySelectorAll('.primary-content p');
-console.log(pInsidePrimaryContent);
 
 /*
 Task 2
@@ -38,11 +27,7 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
-let alertButton = document.getElementById('alertBtn');
 
-alertButton.addEventListener('click', function() {
-    alert("Thanks for visiting Bikes for Refugees!");
-});
 
 /*
 Task 3
@@ -51,11 +36,7 @@ Task 3
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
 
-let chgButton = document.getElementById('bgrChangeBtn');
 
-chgButton.addEventListener('click', function(e) {
-    document.body.style.backgroundColor="red";
-});
 
 
 /*
@@ -64,17 +45,6 @@ Task 4
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
-
-let addText = document.getElementById('addTextBtn');
-
-addText.addEventListener('click', function(){
-    let newParagraph = document.createElement('p');
-    newParagraph.innerText = 'Read more below.';
-
-    let buttonContainer = document.getElementById('button-container');
-    buttonContainer.appendChild(newParagraph);
-});
-
 
 
 
@@ -87,13 +57,3 @@ Task 5
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
 
-let largerLinksBtn = document.getElementById('largerLinksBtn');
-
-largerLinksBtn.addEventListener('click', function(){
-    let links = document.querySelectorAll('a');
-
-    for (let link of links) {
-        let originalSize = link.style.fontSize;  // 1em 3em
-        link.style.fontSize = `${parseFloat(originalSize) + 1}em`;
-    }
-});
